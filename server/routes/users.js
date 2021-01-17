6469be044064f868ecd80041fb3fd697383eb473
EmJee <mart-janroeleveld@outlook.com>
2021-01-17 23:06:04 +0100

    Removed old createUser route and handler

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,16 @@
 import express from 'express'
 
 import {
-	createUser,
 	loginUser,
 	verifyToken,
 	createUserByEmail,
-	verifyEmail
+	verifyEmail,
 } from '../controllers/users.js'
 
 const router = express.Router()
 
 router.post('/createUserByEmail', createUserByEmail)
 router.post('/verifyEmail', verifyEmail)
-router.post('/signup', createUser)
 router.post('/login', loginUser)
 router.post('/verifyToken', verifyToken)
 
